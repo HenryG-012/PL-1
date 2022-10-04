4de98fdebff67bc7863506ddf9692ac953f2526a
HenryG-012 <110263885+HenryG-012@users.noreply.github.com>
2022-10-03 20:24:36 -0400

    Add files via upload

---
+++ src/Welcome.js
@@ -0,0 +1,21 @@
+import React from "react";
+const axios = require('axios');
+
+function Welcome() {
+  function xlm() {
+  axios.get('https://api.coingecko.com/api/v3/simple/price?ids=stellar&vs_currencies=eur&include_24hr_change=true')
+  .then(res => {
+    return('€' + String(res.data.stellar.eur).slice(0, 6) + ' (' + String(res.data.stellar.eur_24h_change).slice(0, 5) + '% 24H)');
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+  return ( <div>
+    <h2> { xlm() } </h2>
+    Welcome To my Page
+  </div>
+)
+  }
+
+export default Welcome;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import Welcome from "./Welcome";
+import About from "./About";
+import Skills from "./Skills";
+
+const App = () => (
+  <div>
+    <div>
+      <Route exact path="/" component={Welcome} />
+      <Route path="/about" component={About} />
+      <Route path="/skills" component={Skills} />
+    </div>
+  </div>
+);
+
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
